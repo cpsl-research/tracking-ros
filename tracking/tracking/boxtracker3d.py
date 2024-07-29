832fc57bc627e6a79dbfbba0e99a1c0a1490503e
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2024-07-29 18:00:40 -0400

    Reference frame (#1)
    * WIP on getting reference frame working for tracking
    
    * WIP on adding boxtracker for multi platform
    
    * Add optional detection transformation
    
    * Make boxtracker a detection based tracker
    
    ---------
    
    Co-authored-by: spencer@primus <spencer.hallyburton@duke.edu>

--- tracking/tracking/boxtracker3d.py
+++ tracking/tracking/boxtracker3d.py
@@ -1,11 +1,16 @@
 import rclpy
 from rclpy.node import Node
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 
+from std_msgs.msg import String
 from vision_msgs.msg import Detection3DArray
 
 from avstack_bridge import Bridge
 from avstack_bridge.detections import DetectionBridge
 from avstack_bridge.tracks import TrackBridge
+from avstack_bridge.transform import do_transform_detection3d
 
 from avstack.modules.tracking.tracker3d import BasicBoxTracker3D
 from avstack_msgs.msg import BoxTrackArray
@@ -14,13 +19,29 @@ from avstack_msgs.msg import BoxTrackArray
 class BoxTracker(Node):
     def __init__(self):
         super().__init__("tracker")
-        self.model = BasicBoxTracker3D()
+        self.declare_parameter("tracking_in_global", False)
+
+        # initialize model
+        self.tracking_in_global = self.get_parameter("tracking_in_global").value
+        self.model = BasicBoxTracker3D(check_reference=False)
 
         qos = rclpy.qos.QoSProfile(
             history=rclpy.qos.QoSHistoryPolicy.KEEP_LAST,
             depth=10,
             reliability=rclpy.qos.QoSReliabilityPolicy.RELIABLE,
             durability=rclpy.qos.QoSDurabilityPolicy.VOLATILE
+        )
+
+        # listen to transform information
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self, qos=qos)
+
+        # subscribe to initialization message (optional)
+        self.subscriber_init = self.create_subscription(
+            String,
+            "/initialization",
+            self.init_callback,
+            qos_profile=qos,
         )
 
         # subscribe to 3d detections
@@ -38,11 +59,35 @@ class BoxTracker(Node):
             qos_profile=qos,
         )
 
+    def init_callback(self, init_msg: String) -> None:
+        if init_msg.data == "reset":
+            self.get_logger().info("Calling reset on box tracker!")
+            self.model.reset()
+
     def dets_callback(self, dets_msg: Detection3DArray) -> BoxTrackArray:
-        dets_avstack = DetectionBridge.detectionarray_to_avstack(dets_msg)
-        platform = Bridge.header_to_reference(dets_msg.header)
-        trks_avstack = self.model(dets_avstack, platform=platform)
-        trks_ros = TrackBridge.avstack_to_tracks(trks_avstack, header=dets_msg.header)
+        # perform reference conversion if needed
+        if self.tracking_in_global:
+            try:
+                tf_world_dets = self.tf_buffer.lookup_transform(
+                    "world",
+                    dets_msg.header.frame_id,
+                    dets_msg.header.stamp,
+                )
+                dets_msg_tf = Detection3DArray()
+                dets_msg_tf.detections = [do_transform_detection3d(det, tf_world_dets) for det in dets_msg.detections]
+                dets_msg_tf.header = tf_world_dets.header
+            except TransformException:
+                self.get_logger().info(
+                    f'Could not transform detections for tracking')
+                return
+        else:
+            dets_msg_tf = dets_msg
+
+        # perform tracking
+        dets_avstack = DetectionBridge.detectionarray_to_avstack(dets_msg_tf)
+        platform = Bridge.header_to_reference(dets_msg_tf.header)
+        trks_avstack = self.model(dets_avstack, platform=platform, check_reference=False)
+        trks_ros = TrackBridge.avstack_to_tracks(trks_avstack, header=dets_msg_tf.header)
         self.publisher_trks.publish(trks_ros)
 
 
---
+++ tracking/tracking/multi_platform_boxtracker3d.py
@@ -0,0 +1,150 @@
+import rclpy
+from rclpy.node import Node
+from message_filters import Subscriber, ApproximateTimeSynchronizer
+
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+from std_msgs.msg import Header, String
+from vision_msgs.msg import Detection3DArray
+from avstack_msgs.msg import BoxTrackArray
+
+from avstack_bridge import Bridge
+from avstack_bridge.geometry import GeometryBridge
+from avstack_bridge.detections import DetectionBridge
+from avstack_bridge.tracks import TrackBridge
+from avstack_bridge.transform import do_transform_detection3d
+
+from avstack.datastructs import DataContainer
+from avstack.modules.tracking.tracker3d import BasicBoxTracker3D
+from avstack.modules.tracking.multisensor import MeasurementBasedMultiTracker
+
+
+class MultiPlatformBoxTracker(Node):
+    def __init__(self, verbose: bool = False):
+        super().__init__("tracker")
+        self.verbose = verbose
+        self.declare_parameter("n_agents", 4)
+
+        # initialize model
+        self.model = MeasurementBasedMultiTracker(
+            tracker=BasicBoxTracker3D(check_reference=False)
+        )
+
+        qos = rclpy.qos.QoSProfile(
+            history=rclpy.qos.QoSHistoryPolicy.KEEP_LAST,
+            depth=10,
+            reliability=rclpy.qos.QoSReliabilityPolicy.RELIABLE,
+            durability=rclpy.qos.QoSDurabilityPolicy.VOLATILE
+        )
+
+        # listen to transform information
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self, qos=qos)
+
+        # subscribe to initialization message (optional)
+        self.subscriber_init = self.create_subscription(
+            String,
+            "/initialization",
+            self.init_callback,
+            qos_profile=qos,
+        )
+
+        # subscribe to 3d tracks from agents
+        self.subscriber_dets = {
+            agent_ID: Subscriber(
+                self,
+                Detection3DArray,
+                f"/agent{agent_ID}/detections_3d",
+                qos_profile=qos,
+            )
+            for agent_ID in range(self.get_parameter("n_agents").value)
+        }
+
+        # synchronize messages from agents
+        self.synchronizer_dets = ApproximateTimeSynchronizer(
+            tuple(self.subscriber_dets.values()), queue_size=10, slop=0.1
+        )
+        self.synchronizer_dets.registerCallback(self.dets_receive)
+
+        # publish tracks from command center
+        self.publisher_trks = self.create_publisher(
+            BoxTrackArray,
+            "tracks_3d",
+            qos_profile=qos,
+        )
+
+    def init_callback(self, init_msg: String) -> None:
+        if init_msg.data == "reset":
+            self.get_logger().info("Calling reset on box tracker!")
+            self.model.reset()
+
+    def dets_receive(self, *args):
+        """Receive approximately synchronized detections
+        
+        Since we set a dynamic number of agents, we have to use star input"""
+        if self.verbose:
+            self.get_logger().info(f"Received {len(args)} detection messages!")
+
+        # convert all tracks to global reference frame
+        dets_global = {}
+        fovs = {}
+        for dets_msg in args:
+            agent = dets_msg.header.frame_id.split("/")[0]
+            
+            # HACK: no fov model for now
+            fovs[agent] = None
+
+            # get track info and convert to global
+            dets_global[agent] = []
+            try:
+                tf_world_dets = self.tf_buffer.lookup_transform(
+                    "world",
+                    dets_msg.header.frame_id,
+                    dets_msg.header.stamp,
+                )
+                dets_global[agent] = DataContainer(
+                    frame=0,
+                    timestamp=Bridge.rostime_to_time(dets_msg.header.stamp),
+                    data=[
+                        DetectionBridge.detection_to_avstack(
+                            do_transform_detection3d(boxdet, tf_world_dets),
+                        )
+                        for boxdet in dets_msg.detections
+                    ],
+                    source_identifier=agent,
+                )
+            except TransformException:
+                self.get_logger().info(
+                    f'Could not transform detections for multi-platform tracking')
+                return
+            finally:
+                last_stamp = dets_msg.header.stamp 
+
+        # perform tracking in global
+        trks_avstack = self.model(dets_global, fovs=fovs, check_reference=False)
+        header_out = Header(frame_id="world", stamp=last_stamp)
+        trks_ros = TrackBridge.avstack_to_tracks(trks_avstack, header=header_out)
+        self.publisher_trks.publish(trks_ros)
+
+        if self.verbose:
+            self.get_logger().info(f"Published {len(trks_avstack)} tracks!")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    tracker = MultiPlatformBoxTracker()
+
+    rclpy.spin(tracker)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    tracker.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
